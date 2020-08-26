8ccd7263f307e8da87c78123148525e1c7ba4b52
Khurshid <shamsun.com@gmail.com>
2020-08-26 18:00:00 +0900

    Initial Commit

---
+++ android/app/src/main/kotlin/com/pdp/pdpui7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pdp.pdpui7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFF018197),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    child:
+                        Image(image: AssetImage('assets/images/amazon_logo.png')),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.mic, color: Colors.white),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.shopping_cart, color: Colors.white),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              color: Color(0xFF018197),
+              child: Container(
+                height: 50,
+                padding: EdgeInsets.only(left: 10, right: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1, //                   <--- border width here
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "What are you looking for?",
+                          border: InputBorder.none,
+                          icon: Icon(
+                            Icons.search,
+                            color: Color(0xFF018197),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.camera_alt, color: Color(0xFF018197)),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 10,right: 10),
+                    color: Colors.blueGrey,
+                    height: 45,
+                    child: Row(
+                      children: [
+                        Icon(Icons.location_on,color: Colors.white,),
+                        SizedBox(width: 5,),
+                        Text("Deliver to Korea, Republic of",style: TextStyle(color: Colors.white),),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 140,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          width: 180,
+                          color: Colors.white,
+                          child: Center(
+                            child: Text("We ship 45million products",style: TextStyle(fontSize: 16),),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.white,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(topLeft: Radius.circular(70),bottomLeft: Radius.circular(70)),
+                                image: DecorationImage(
+                                  image: AssetImage("assets/images/image_1.jpeg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    padding: EdgeInsets.only(left: 16,right: 16),
+                    height: 160,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Sign in for the best experience",style: TextStyle(color: Colors.black,fontSize: 18),),
+                        SizedBox(height: 15,),
+                        Container(
+                          width: double.infinity,
+                          height: 50,
+                          color: Colors.orange,
+                          child: Center(
+                            child: Text("Sign in",style: TextStyle(fontSize: 18),),
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+                        Text("Create an account",style: TextStyle(color: Colors.blueAccent,fontSize: 18),),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    height: 320,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Deal of the Day",style: TextStyle(color: Colors.black,fontSize: 22),)
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    height: 320,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Best sellers in Electronics",style: TextStyle(color: Colors.black,fontSize: 22),)
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    height: 320,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Top products in Camera",style: TextStyle(color: Colors.black,fontSize: 22),)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
